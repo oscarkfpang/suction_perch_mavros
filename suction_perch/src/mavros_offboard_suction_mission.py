34be20597119930a4a8f94849b1ac126712b4983
oscarkfpang <31898141+oscarkfpang@users.noreply.github.com>
2022-01-10 21:16:41 +0800

    Update mavros service and essential topics

--- suction_perch/src/mavros_offboard_suction_mission.py
+++ suction_perch/src/mavros_offboard_suction_mission.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 from __future__ import division
 
 import rospy
@@ -6,9 +6,14 @@ import math
 import numpy as np
 from geometry_msgs.msg import PoseStamped, Quaternion
 from mavros_msgs.msg import ParamValue
+from mavros_msgs.msg import Altitude, ExtendedState, HomePosition, ParamValue, State, \
+                            WaypointList
+from mavros_msgs.srv import CommandBool, ParamGet, ParamSet, SetMode, SetModeRequest, WaypointClear, \
+                            WaypointPush, CommandTOL
+from sensor_msgs.msg import NavSatFix, Imu
 #from mavros_test_common import MavrosTestCommon
-#from pymavlink import mavutil
-#from six.moves import xrange
+from pymavlink import mavutil
+from six.moves import xrange
 from std_msgs.msg import Header
 from threading import Thread
 from tf.transformations import quaternion_from_euler
@@ -26,16 +31,137 @@ class MavrosOffboardSuctionMission():
 
     def __init__(self, radius=0.2, ):
         self.radius = radius
+        self.mission_cnt = 0
+        self.mission_pos = ((0, 0, 0) , (0, 0, 5.8), (1, 0, 5.8) ) #, (1, 1.5, 5.8), (0, 1.5, 5.8), (0, 0, 5))
+        # ROS services
+        service_timeout = 30
+        rospy.loginfo("waiting for ROS services")
+        try:
+            rospy.wait_for_service('mavros/param/get', service_timeout)
+            rospy.wait_for_service('mavros/param/set', service_timeout)
+            rospy.wait_for_service('mavros/cmd/arming', service_timeout)
+            rospy.wait_for_service('mavros/mission/push', service_timeout)
+            rospy.wait_for_service('mavros/mission/clear', service_timeout)
+            rospy.wait_for_service('mavros/set_mode', service_timeout)
+            rospy.wait_for_service('mavros/cmd/takeoff', service_timeout)
+            rospy.wait_for_service('mavros/cmd/land', service_timeout)
+            rospy.loginfo("ROS services are up")
+        except rospy.ROSException:
+            self.fail("failed to connect to services")
+        
+        # mavros service
+        self.set_arming_srv = rospy.ServiceProxy('mavros/cmd/arming',
+                                                 CommandBool)
+        self.set_mode_srv = rospy.ServiceProxy('mavros/set_mode', SetMode)
+        self.set_takeoff_srv = rospy.ServiceProxy('mavros/cmd/takeoff', CommandTOL)
+        self.set_land_srv = rospy.ServiceProxy('mavros/cmd/land', CommandTOL)
+        
+        # mavros topics
+        self.altitude = Altitude()
+        self.extended_state = ExtendedState()
+        #self.global_position = NavSatFix()
+        self.imu_data = Imu()
+        self.home_position = HomePosition()
+        self.local_position = PoseStamped()
+        self.mission_wp = WaypointList()
+        self.state = State()
+        
         self.pos = PoseStamped()
 
         self.pos_setpoint_pub = rospy.Publisher(
             'mavros/setpoint_position/local', PoseStamped, queue_size=1)
+
+        self.sub_topics_ready = {
+            key: False
+            for key in [
+                'alt', 'ext_state', 'state', 'imu', 'local_pos'
+            ]
+        }
+
+        # ROS subscribers
+        self.alt_sub = rospy.Subscriber('mavros/altitude', Altitude,
+                                        self.altitude_callback)
+        self.ext_state_sub = rospy.Subscriber('mavros/extended_state',
+                                              ExtendedState,
+                                              self.extended_state_callback)
+        self.imu_data_sub = rospy.Subscriber('mavros/imu/data',
+                                               Imu,
+                                               self.imu_data_callback)
+        self.state_sub = rospy.Subscriber('mavros/state', State,
+                                          self.state_callback)
+        self.local_pos_sub = rospy.Subscriber('mavros/local_position/pose',
+                                              PoseStamped,
+                                              self.local_position_callback)
 
         # send setpoints in seperate thread to better prevent failsafe
         self.pos_thread = Thread(target=self.send_pos, args=())
         self.pos_thread.daemon = True
         self.pos_thread.start()
 
+    def altitude_callback(self, data):
+        self.altitude = data
+
+        # amsl has been observed to be nan while other fields are valid
+        if not self.sub_topics_ready['alt'] and not math.isnan(data.amsl):
+            self.sub_topics_ready['alt'] = True
+
+    def extended_state_callback(self, data):
+        if self.extended_state.vtol_state != data.vtol_state:
+            rospy.loginfo("VTOL state changed from {0} to {1}".format(
+                mavutil.mavlink.enums['MAV_VTOL_STATE']
+                [self.extended_state.vtol_state].name, mavutil.mavlink.enums[
+                    'MAV_VTOL_STATE'][data.vtol_state].name))
+
+        if self.extended_state.landed_state != data.landed_state:
+            rospy.loginfo("landed state changed from {0} to {1}".format(
+                mavutil.mavlink.enums['MAV_LANDED_STATE']
+                [self.extended_state.landed_state].name, mavutil.mavlink.enums[
+                    'MAV_LANDED_STATE'][data.landed_state].name))
+
+        self.extended_state = data
+
+        if not self.sub_topics_ready['ext_state']:
+            self.sub_topics_ready['ext_state'] = True
+
+
+    def imu_data_callback(self, data):
+        self.imu_data = data
+
+        if not self.sub_topics_ready['imu']:
+            self.sub_topics_ready['imu'] = True
+
+
+    def state_callback(self, data):
+        if self.state.armed != data.armed:
+            rospy.loginfo("armed state changed from {0} to {1}".format(
+                self.state.armed, data.armed))
+
+        if self.state.connected != data.connected:
+            rospy.loginfo("connected changed from {0} to {1}".format(
+                self.state.connected, data.connected))
+
+        if self.state.mode != data.mode:
+            rospy.loginfo("mode changed from {0} to {1}".format(
+                self.state.mode, data.mode))
+
+        if self.state.system_status != data.system_status:
+            rospy.loginfo("system_status changed from {0} to {1}".format(
+                mavutil.mavlink.enums['MAV_STATE'][
+                    self.state.system_status].name, mavutil.mavlink.enums[
+                        'MAV_STATE'][data.system_status].name))
+
+        self.state = data
+
+        # mavros publishes a disconnected state message on init
+        if not self.sub_topics_ready['state'] and data.connected:
+            self.sub_topics_ready['state'] = True
+
+    def local_position_callback(self, data):
+        self.local_position = data
+
+        if not self.sub_topics_ready['local_pos']:
+            self.sub_topics_ready['local_pos'] = True           
+            
     #
     # Helper methods
     #
@@ -43,6 +169,9 @@ class MavrosOffboardSuctionMission():
         rate = rospy.Rate(10)  # Hz
         self.pos.header = Header()
         self.pos.header.frame_id = "base_footprint"
+        self.pos.pose.position.x = self.mission_pos[self.mission_cnt][0]
+        self.pos.pose.position.y = self.mission_pos[self.mission_cnt][1]
+        self.pos.pose.position.z = self.mission_pos[self.mission_cnt][2]
 
         while not rospy.is_shutdown():
             self.pos.header.stamp = rospy.Time.now()
@@ -52,32 +181,120 @@ class MavrosOffboardSuctionMission():
             except rospy.ROSInterruptException:
                 pass
 
-    def run(self):
-        # make sure the simulation is ready to start the mission
-        self.wait_for_mission_topics(60)
-        self.wait_for_landed_state(mavutil.mavlink.MAV_LANDED_STATE_ON_GROUND,
-                                   10, -1)
-        return
+    def set_arm(self, arm, timeout):
+        """arm: True to arm or False to disarm, timeout(int): seconds"""
+        rospy.loginfo("setting FCU arm: {0}".format(arm))
+        old_arm = self.state.armed
+        loop_freq = 1  # Hz
+        rate = rospy.Rate(loop_freq)
+        arm_set = False
+        for i in xrange(timeout * loop_freq):
+            if self.state.armed == arm:
+                arm_set = True
+                rospy.loginfo("set arm success | seconds: {0} of {1}".format(
+                    i / loop_freq, timeout))
+                break
+            else:
+                try:
+                    res = self.set_arming_srv(arm)
+                    if not res.success:
+                        rospy.logerr("failed to send arm command")
+                except rospy.ServiceException as e:
+                    rospy.logerr(e)
 
-    def is_at_position(self, x, y, z, offset):
+            try:
+                rate.sleep()
+            except rospy.ROSException as e:
+                pass
+                #self.fail(e)
+
+        self.assertTrue(arm_set, (
+            "failed to set arm | new arm: {0}, old arm: {1} | timeout(seconds): {2}".
+            format(arm, old_arm, timeout)))
+        return arm_set
+
+    def set_mode(self, mode, timeout):
+        """mode: PX4 mode string, timeout(int): seconds"""
+        rospy.loginfo("setting FCU mode: {0}".format(mode))
+        old_mode = self.state.mode
+        loop_freq = 1  # Hz
+        rate = rospy.Rate(loop_freq)
+        mode_set = False
+        for i in xrange(timeout * loop_freq):
+            if self.state.mode == mode:
+                mode_set = True
+                rospy.loginfo("set mode success | seconds: {0} of {1}".format(
+                    i / loop_freq, timeout))
+                break
+            else:
+                try:
+                    res = self.set_mode_srv(0, mode)  # 0 is custom mode
+                    if not res.mode_sent:
+                        rospy.logerr("failed to send mode command")
+                except rospy.ServiceException as e:
+                    rospy.logerr(e)
+
+            try:
+                rate.sleep()
+            except rospy.ROSException as e:
+                pass
+                #self.fail(e)
+
+        self.assertTrue(mode_set, (
+            "failed to set mode | new mode: {0}, old mode: {1} | timeout(seconds): {2}".
+            format(mode, old_mode, timeout)))
+
+    def takeoff(self, timeout=10):
+        """mode: PX4 mode string, timeout(int): seconds"""
+        #rospy.loginfo("setting FCU mode: {0}".format(mode))
+        loop_freq = 1  # Hz
+        rate = rospy.Rate(loop_freq)
+        take_off = False
+        for i in xrange(timeout * loop_freq):
+            if not take_off:
+                try:
+                    self.set_takeoff_srv(altitude = 3.0)
+                    take_off = True
+                    rospy.loginfo("takeoff successful!")
+                    break
+                except rospy.ServiceException as e:
+                    rospy.loginfo("takeoff call failed.. try again")
+           
+            try:
+                rate.sleep()
+            except rospy.ROSException as e:
+                pass
+                #self.fail(e)
+        if take_off:
+            rospy.loginfo("Waiting for take off 5 sec")
+            rospy.sleep(5)
+        return take_off
+
+    def assertTrue(self, assertTrue, fail_msg):
+        if not assertTrue:
+            rospy.loginfo(fail_msg)
+
+    def is_at_position(self, offset):
         """offset: meters"""
         rospy.logdebug(
             "current position | x:{0:.2f}, y:{1:.2f}, z:{2:.2f}".format(
                 self.local_position.pose.position.x, self.local_position.pose.
                 position.y, self.local_position.pose.position.z))
 
-        desired = np.array((x, y, z))
+        desired = np.array((self.mission_pos[self.mission_cnt][0], 
+                            self.mission_pos[self.mission_cnt][1], 
+                            self.mission_pos[self.mission_cnt][2]))
         pos = np.array((self.local_position.pose.position.x,
                         self.local_position.pose.position.y,
                         self.local_position.pose.position.z))
         return np.linalg.norm(desired - pos) < offset
 
-    def reach_position(self, x, y, z, timeout):
+    def reach_position(self, timeout):
         """timeout(int): seconds"""
         # set a position setpoint
-        self.pos.pose.position.x = x
-        self.pos.pose.position.y = y
-        self.pos.pose.position.z = z
+        x = self.mission_pos[self.mission_cnt][0]
+        y = self.mission_pos[self.mission_cnt][1]
+        z = self.mission_pos[self.mission_cnt][2]
         rospy.loginfo(
             "attempting to reach position | x: {0}, y: {1}, z: {2} | current position x: {3:.2f}, y: {4:.2f}, z: {5:.2f}".
             format(x, y, z, self.local_position.pose.position.x,
@@ -95,9 +312,7 @@ class MavrosOffboardSuctionMission():
         rate = rospy.Rate(loop_freq)
         reached = False
         for i in xrange(timeout * loop_freq):
-            if self.is_at_position(self.pos.pose.position.x,
-                                   self.pos.pose.position.y,
-                                   self.pos.pose.position.z, self.radius):
+            if self.is_at_position(self.radius):
                 rospy.loginfo("position reached | seconds: {0} of {1}".format(
                     i / loop_freq, timeout))
                 reached = True
@@ -106,13 +321,68 @@ class MavrosOffboardSuctionMission():
             try:
                 rate.sleep()
             except rospy.ROSException as e:
-                self.fail(e)
+                #self.fail(e)
+                pass
 
         self.assertTrue(reached, (
             "took too long to get to position | current position x: {0:.2f}, y: {1:.2f}, z: {2:.2f} | timeout(seconds): {3}".
             format(self.local_position.pose.position.x,
                    self.local_position.pose.position.y,
                    self.local_position.pose.position.z, timeout)))
+        return reached
+
+    def run_mission(self):
+        # make sure the simulation is ready to start the mission
+        self.wait_for_mission_topics(60)
+        self.wait_for_landed_state(mavutil.mavlink.MAV_LANDED_STATE_ON_GROUND,
+                                   10, -1)
+        self.set_mode("OFFBOARD", 5)
+        if self.set_arm(True, 5) and self.takeoff(10):
+            self.mission_cnt += 1
+        else:
+            return -1
+        
+        while self.mission_cnt < len(self.mission_pos):
+            if self.reach_position(20):
+                self.mission_cnt += 1
+            else:
+                break
+                      
+        rospy.loginfo("Mission completed... wait for 3 sec before landing.")          
+        rospy.sleep(3)
+        self.set_arm(False, 5)
+        self.set_mode("STABILIZED", 5)
+        return
+
+    #
+    # Test method
+    #
+    def test_posctl(self):
+        """Test offboard position control"""
+
+        # make sure the simulation is ready to start the mission
+        self.wait_for_mission_topics(60)
+        self.wait_for_landed_state(mavutil.mavlink.MAV_LANDED_STATE_ON_GROUND,
+                                   10, -1)
+
+        self.log_topic_vars()
+        # exempting failsafe from lost RC to allow offboard
+        #rcl_except = ParamValue(1<<2, 0.0)
+        #self.set_param("COM_RCL_EXCEPT", rcl_except, 5)
+        self.set_mode("OFFBOARD", 5)
+        self.set_arm(True, 5)
+
+        rospy.loginfo("run mission")
+        positions = ((0, 0, 0) , (0, 0, 0.1) ) #, (1, 0, 1.8), (1, 1.5, 1.8), (0, 1.5, 1.8), (0, 0, 0))
+
+        for i in xrange(len(positions)):
+            self.reach_position(positions[i][0], positions[i][1],
+                                positions[i][2], 30)
+
+        self.set_mode("AUTO.LAND", 5)
+        self.wait_for_landed_state(mavutil.mavlink.MAV_LANDED_STATE_ON_GROUND,
+                                   45, 0)
+        self.set_arm(False, 5)
 
     # define a new method similar to the original wait_for_mission for real-life operation
     # wait until all mavros topics to be ready
@@ -147,40 +417,39 @@ class MavrosOffboardSuctionMission():
             "failed to hear from all subscribed mavros topics | topic ready flags: {0} | timeout(seconds): {1}".
             format(self.sub_topics_ready, timeout)))
 
-    #
-    # Test method
-    #
-    def test_posctl(self):
-        """Test offboard position control"""
+    def wait_for_landed_state(self, desired_landed_state, timeout, index):
+        rospy.loginfo("waiting for landed state | state: {0}, index: {1}".
+                      format(mavutil.mavlink.enums['MAV_LANDED_STATE'][
+                          desired_landed_state].name, index))
+        loop_freq = 10  # Hz
+        rate = rospy.Rate(loop_freq)
+        landed_state_confirmed = False
+        for i in xrange(timeout * loop_freq):
+            if self.extended_state.landed_state == desired_landed_state:
+                landed_state_confirmed = True
+                rospy.loginfo("landed state confirmed | seconds: {0} of {1}".
+                              format(i / loop_freq, timeout))
+                break
 
-        # make sure the simulation is ready to start the mission
-        self.wait_for_mission_topics(60)
-        self.wait_for_landed_state(mavutil.mavlink.MAV_LANDED_STATE_ON_GROUND,
-                                   10, -1)
+            try:
+                rate.sleep()
+            except rospy.ROSException as e:
+                self.fail(e)
 
-        self.log_topic_vars()
-        # exempting failsafe from lost RC to allow offboard
-        #rcl_except = ParamValue(1<<2, 0.0)
-        #self.set_param("COM_RCL_EXCEPT", rcl_except, 5)
-        self.set_mode("OFFBOARD", 5)
-        self.set_arm(True, 5)
-
-        rospy.loginfo("run mission")
-        positions = ((0, 0, 0) , (0, 0, 0.1) ) #, (1, 0, 1.8), (1, 1.5, 1.8), (0, 1.5, 1.8), (0, 0, 0))
-
-        for i in xrange(len(positions)):
-            self.reach_position(positions[i][0], positions[i][1],
-                                positions[i][2], 30)
-
-        self.set_mode("AUTO.LAND", 5)
-        self.wait_for_landed_state(mavutil.mavlink.MAV_LANDED_STATE_ON_GROUND,
-                                   45, 0)
-        self.set_arm(False, 5)
+        self.assertTrue(landed_state_confirmed, (
+            "landed state not detected | desired: {0}, current: {1} | index: {2}, timeout(seconds): {3}".
+            format(mavutil.mavlink.enums['MAV_LANDED_STATE'][
+                desired_landed_state].name, mavutil.mavlink.enums[
+                    'MAV_LANDED_STATE'][self.extended_state.landed_state].name,
+                   index, timeout)))
 
 
 if __name__ == '__main__':
     rospy.init_node('suction_mission_node')
-    suction_misssion = MavrosOffboardSuctionMission()
+    suction_mission = MavrosOffboardSuctionMission()
+    suction_mission.run_mission()
+    #rospy.spin()
+    '''
     try:
         rate = rospy.Rate(100)
         while not rospy.is_shutdown():
@@ -189,3 +458,4 @@ if __name__ == '__main__':
     except rospy.ROSInterruptException:
         rospy.loginfo("main has finished! \n")
 
+    '''
